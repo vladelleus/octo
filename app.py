8d9982170a7728a41867e611b3d66e7030475f36
Vlad Matsapura <vladelleus20001@gmail.com>
2022-08-31 13:49:01 +0300

    adding new version of app

--- app.py
+++ app.py
@@ -1,9 +1,21 @@
-from flask import Flask
+from flask import Flask, redirect, url_for, request
 app = Flask(__name__)
  
-@app.route('/hello/<name>')
-def hello_name(name):
-   return 'Hello %s!' % name
+ 
+@app.route('/success/<name>')
+def success(name):
+    return 'welcome %s' % name
+ 
+ 
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    if request.method == 'POST':
+        user = request.form['nm']
+        return redirect(url_for('success', name=user))
+    else:
+        user = request.args.get('nm')
+        return redirect(url_for('success', name=user))
+ 
  
 if __name__ == '__main__':
-   app.run()
+    app.run(debug=True)
